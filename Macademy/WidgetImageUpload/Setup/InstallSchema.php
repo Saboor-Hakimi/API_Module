58de41e70fcb3d9c8a69bc4a923e370612e250e7
Saboor-Hakimi <44523544+Saboor-Hakimi@users.noreply.github.com>
2021-07-17 10:07:59 +0430

    Update InstallSchema.php
    Fixed the Table class not exists error by using (use Magento\Framework\DB\Ddl\Table;)

--- Macademy/WidgetImageUpload/Setup/InstallSchema.php
+++ Macademy/WidgetImageUpload/Setup/InstallSchema.php
@@ -6,6 +6,7 @@ use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\DB\Ddl\Table;
 class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface{
     public function install(SchemaSetupInterface $setup,ModuleContextInterface $context){
         $setup->startSetup();
